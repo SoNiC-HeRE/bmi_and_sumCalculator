9c1c5f1740a8d63488237d286e65f72a31087700
Sriyansh Shivam <sriyanshshivam1@gmail.com>
2023-06-11 14:13:05 +0530

    Updated Code

---
+++ calculator.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen("3000",()=>{
+    console.log("Server Started");
+});
+
+app.post("/",(req,res)=>{
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    var result = num1 + num2;
+    res.send("The result of the calculation is: "+ result);
+});
+
+app.post("/bmi",(req,res)=>{
+    var height = parseFloat(req.body.height);
+    var weight = parseFloat(req.body.weight);
+    var bmi = weight/(height*height);
+    res.send("Your BMI is: "+ bmi);
+})
+
+app.get("/bmi",(req,res)=>{
+    res.sendFile(__dirname + "/bmiCalculator.html");
+})
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname + "/index.html");
+});
+
